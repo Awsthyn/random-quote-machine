e6ce80924e1be1b904a9cad538ddb671ece8d514
Awsthyn <aguswagner008gmail.com>
2020-09-13 18:39:50 -0300

    Updates

---
+++ precache-manifest.8b955b28a9aa2171bd63d6b143845e3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfde72d88a10fb98bd9a63ed1783938",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "803d01c217716a1a9fe6",
+    "url": "/random-quote-machine/static/css/main.c5b57238.chunk.css"
+  },
+  {
+    "revision": "2384b7aafcdc87349cdc",
+    "url": "/random-quote-machine/static/js/2.8a21fb19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.8a21fb19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "803d01c217716a1a9fe6",
+    "url": "/random-quote-machine/static/js/main.f04e921f.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  },
+  {
+    "revision": "6620c8ae2cac90067a9aa78339bb808d",
+    "url": "/random-quote-machine/static/media/twitter-brands.6620c8ae.svg"
+  }
+]);
